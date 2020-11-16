deb677aec852226c56d1ba08f0a1857525de83f0
Lee Seong Won <syanggu@naver.com>
2020-11-16 18:43:05 +0900

    while do while go to 2

--- c_ex8.cpp
+++ c_ex8.cpp
@@ -1,65 +1,65 @@
 #include <stdio.h>
 
 int main() {
-  // int choice;
+  int choice;
 
-  // makeChoice:
+  makeChoice:
 
-  // printf("새 게임 : 1\n");
-  // printf("불러오기 : 2\n");
-  // printf("설정 : 3\n");
-  // printf("크레딧 : 4\n");
+  printf("새 게임 : 1\n");
+  printf("불러오기 : 2\n");
+  printf("설정 : 3\n");
+  printf("크레딧 : 4\n");
 
-  // scanf("%d", &choice);
+  scanf("%d", &choice);
 
-  // switch(choice) {
-  //   case(1): printf("새 게임 : 1\n"); break;
-  //   case(2): printf("불러오기 : 2\n"); break;
-  //   case(3): printf("설정 : 3\n"); break;
-  //   case(4): printf("크레딧 : 4\n"); break;
-  //   default: 
-  //     printf("숫자를 다시입력해주세요\n");
-  //     // go to
-  //     goto makeChoice; // 쓰면 코드가 더러워짐 (스파게티코드)
-  //     break;
-  // }
+  switch(choice) {
+    case(1): printf("새 게임 : 1\n"); break;
+    case(2): printf("불러오기 : 2\n"); break;
+    case(3): printf("설정 : 3\n"); break;
+    case(4): printf("크레딧 : 4\n"); break;
+    default: 
+      printf("숫자를 다시입력해주세요\n");
+      // go to
+      goto makeChoice; // 쓰면 코드가 더러워짐 (스파게티코드)
+      break;
+  }
 
-  // // while
-  // int i = 1;
+  // while
+  int i = 1;
 
-  // repeat:
-  // printf("%d\n", i);
-  // if (i < 10) {
-  //   i++;
-  //   goto repeat; // 긍정적으로 쓰일 경우
-  // }
+  repeat:
+  printf("%d\n", i);
+  if (i < 10) {
+    i++;
+    goto repeat; // 긍정적으로 쓰일 경우
+  }
 
-  // // 반복문 (while)
-  // int i = 1;
+  // 반복문 (while)
+  int i = 1;
 
-  // while (i <= 10) {
-  //   printf("%d\n", i);
-  //   i++;
-  // }
+  while (i <= 10) {
+    printf("%d\n", i);
+    i++;
+  }
   
-  // // do while
+  // do while
 
-  // int i = 1;
-  // do {
-  //   printf("%d\n", i);
-  //   i++;
-  // } while (i <= 10);
+  int i = 1;
+  do {
+    printf("%d\n", i);
+    i++;
+  } while (i <= 10);
 
-  // // 무한 루프
-  // int i = 1;
+  // 무한 루프
+  int i = 1;
 
-  // while (i <= 10) {
-  //   int i = 1;
-  //   while (true) {
-  //     printf("%d", i);
-  //     i++;
-  //   }
-  // }
+  while (i <= 10) {
+    int i = 1;
+    while (true) {
+      printf("%d", i);
+      i++;
+    }
+  }
 
   // do while 가 쓰일 경우 (무조건 한번은 실행되어야 할때)
   
